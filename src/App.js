00c884b6205f0a372cf429d1771845d24cdf537b
sarahrami <sarahrami97@gmail.com>
2023-10-11 23:29:02 -0400

    react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import React, { Component } from 'react';
-import './App.css';
-import Sidebar from './components/sidebar'
-import Introduction from './components/introduction'
-import About from './components/about'
-import Timeline from './components/timeline'
+// import React, { Component } from 'react';
+// import './App.css';
+// import Sidebar from './components/sidebar'
+// import Introduction from './components/introduction'
+// import About from './components/about'
+// import Timeline from './components/timeline'
 
-class App extends Component {
-  render() {
-    return (
-      <div id="colorlib-page">
-        <div id="container-wrap">
-         	<Sidebar></Sidebar>
-          <div id="colorlib-main">
-            <Introduction></Introduction>
-            <About></About>
-            <Timeline></Timeline>
-          </div>
-      	</div>
-      </div>
-    );
-  }
+function App(){
+  return(
+    <h1>Welcome to DevOps - 101376641 Sarah Moustafa</h1>
+  )
 }
 
 export default App;
